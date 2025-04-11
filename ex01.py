bae4710178b6d20b46894b8c0d84c12deaa9954e
Ezequiel de Oliveira Santos <oliveiraezequiel474@gmail.com>
2025-04-11 10:16:47 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,11 @@
+#               0         1         2             3           4
+linguagens = ['Python', 'Java', 'Javascript', 'Typescript', 'Go']
+
+# a)
+print(f'Resposta A) -> {linguagens[3]}')
+
+# b)
+print(f'Resposta B) -> {linguagens[2]}')
+
+# c)
+print(f'Resposta C) -> {linguagens[0]}')
