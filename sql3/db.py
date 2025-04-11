8997a0bd9182aee66a8ac3217e6fe409b1348e99
Ezequiel de Oliveira Santos <oliveiraezequiel474@gmail.com>
2025-04-11 12:54:25 -0300

    Add files via upload

---
+++ sql3/db.py
@@ -0,0 +1,10 @@
+import mysql.connector as mysql
+
+
+conn = mysql.connect(
+    host='roo27.h.filess.io',
+    port=61002,
+    user='testdb_skinskinit',
+    password='21b3f71f7ce8420cc37c28e53a0f30e97e592ab5',
+    database='testdb_skinskinit'
+)
